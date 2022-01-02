4d07bbd08b9636e0cfb0633e747824fc546beb6c
Lohithkumaras <89640966+Lohithkumaras@users.noreply.github.com>
2022-01-02 17:46:45 +0530

    Update PrimePrint.js

--- PrimePrint.js
+++ PrimePrint.js
@@ -1,27 +1,26 @@
 
-const num = 24;
-const isPrime = num => {
-   let count = 2;
-   while(count < (num / 2)+1){
-      if(num % count !== 0){
-         count++;
-         continue;
-      };
-      return false;
-   };
-   return true;
-};
-const primeUpto = num => {
-   if(num < 2){
-      return [];
-   };
-   const res = [2];
-   for(let i = 3; i <= num; i++){
-      if(!isPrime(i)){
-         continue;
-      };
-      res.push(i);
-   };
-   return res;
-};
-console.log(primeUpto(num));
+var num=parseInt(prompt("Enter the Number"));
+
+for(var i=0;i<=num;i++)
+{
+    if(CheckPrime(i))
+    {
+        console.log(i);
+    }
+}
+
+function CheckPrime(n)
+{
+
+if(n==0||n==1)
+return false;
+else if(n==2)
+return true;
+else{
+    for(var k=2;k<n;k++)
+    {
+        if(n%k!=0)
+        return true;
+    }
+}
+}
